55cbf06bf85c5415c9de9b2d0702ba7b83dc701c
Monkeybeam3 <85470817+Monkeybeam3@users.noreply.github.com>
2022-06-22 17:51:40 -0600

    new and updated version

--- passwordgen.py
+++ passwordgen.py
@@ -1,26 +1,56 @@
 #Password generator
+import sys
 import time
 import random
+def looppassword():
+    print("Making some more passwords... These will all be charecter passwords...")
+    original_stdout = sys.stdout
+    passwordlistf = []
+    for i in range(48):
+            randomnum = random.randrange(33,126)
+            asciipasword = chr(randomnum)
+            asciipasswordstring = str(asciipasword)
+            passwordlistf.append(asciipasswordstring)
+    with open('passwords.txt', 'a') as f:
+        sys.stdout = f # Change the standard output to the file we created.
+        print(*passwordlistf , sep='')
+        sys.stdout = original_stdout
+    looppassword()
 def createpassword():
+    howlong = input("How many charecters should the password be? (in number form please): ")
+    howlongint = int(howlong)
+    original_stdout = sys.stdout
     passwordlist = []
-    for q in range(16):
+    for q in range(howlongint):
             randomnum = random.randrange(33,126)
             asciipasword = chr(randomnum)
             asciipasswordstring = str(asciipasword)
             passwordlist.append(asciipasswordstring)
-    print("Your Password is: ",*passwordlist , sep='')
-    print("Your password is being automatically saved.")
-    print("By the way, this file will be printed to wherever this python file is.")
+    print("Your password is " , *passwordlist , sep='')
     time.sleep(2)
+    print("Your password is being automatically saved to a file called passwords.txt.")
+    time.sleep(2)
+    print("By the way, this file will be printed to wherever this python file is.")
+    time.sleep(2)      
     with open('passwords.txt', 'w') as f:
-        f.writelines(["%s\n" % item  for item in passwordlist])
+        sys.stdout = f # Change the standard output to the file we created.
+        print(*passwordlist , sep='')
+        sys.stdout = original_stdout
     print("Saved!")
-    
+    again = input("Would you like to create more passwords? (Y/n): ")
+    if again == "Y" or "y":
+        print("Ok!")
+        looppassword()
+    elif again == "N" or "n":
+        print("Goodbye!")
+    elif again != "Y" or "y" or "N" or "n":
+        print("Automaticly creating a new password...")
 def passwordgen():
-    print("This will automaticly be a 16 digit password.")
+    print("This will automaticly create a password.")
     print("Starting now...")
     time.sleep(3)
     createpassword()
 print("Hello and welcome to the ACME automatic password generator.")
 time.sleep(2)
-passwordgen()
+createpassword()
+#REMEMBER TO CHANGE THIS
